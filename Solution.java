037649c5357b75bf0061de8262fb60e9c3eb03ff
sdim-ua <ingersol@yandex.ua>
2019-06-14 10:33:14 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+/* 
+От школьника до квалифицированного раба
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student {
+
+    }
+
+    public class Slave extends Worker {
+
+    }
+
+}
